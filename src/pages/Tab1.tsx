0ece7f0b47347137d3ea872e9de9f70ac00b75b4
Alan Montgomery <alan.montgomery@tascomi.com>
2021-10-26 16:36:16 +0100

    Initial

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonItem, IonLabel, IonPage, IonTitle, IonToggle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -16,7 +16,15 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+        <IonButton color="alans-color">
+          Button
+        </IonButton>
+
+        <IonItem>
+          <IonLabel>Toggle</IonLabel>
+          <IonToggle />
+        </IonItem>
       </IonContent>
     </IonPage>
   );
